fdf09017614ee9242638b75cfd7833fd80e51c6b
Josh Tripoli <jct324@lehigh.edu>
2023-02-10 17:13:56 -0500

    Complete sprint course assignment updating

---
+++ fellowData/REL161.js
@@ -0,0 +1,18 @@
+export const REL161 = `id,assignment,name,pronouns,hometown,year,majorMinor,info,email
+missing,"REL 161","Schott",‎,"",‎,"","",mwd223@lehigh.edu
+hideMe,"HIST 001","",‎,"",‎,"","",mwd223@lehigh.edu
+hideMe,"HIST 001","",‎,"",‎,"","",mwd223@lehigh.edu`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
