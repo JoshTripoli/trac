358863f8d5d552a3693a06212695a5fc43fba869
Josh Tripoli <jct324@lehigh.edu>
2023-02-10 15:55:07 -0500

    bios 41 and bus 3

--- fellowData/ENGL1.js
+++
@@ -1,4 +0,0 @@
-export const ENGL1 = `id,assignment,name,pronouns,hometown,year,majorMinor,info,email
-missing,"ENGL1","Horan",‎,"",‎,"","",mwd223@lehigh.edu
-missing,"ENGL1","Alkhatib",‎,"",‎,"","",mwd223@lehigh.edu
-hideMe,"BUS 003","",‎,"",‎,"","",mwd223@lehigh.edu`;
