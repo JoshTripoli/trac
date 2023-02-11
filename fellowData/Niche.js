30cc12a47ce24a0dd9d1d4ef8785609a7c0d937f
Josh Tripoli <jct324@lehigh.edu>
2023-02-11 01:17:23 -0500

    Added blank profile and styled less than three

--- fellowData/Niche.js
+++ fellowData/Niche.js
@@ -1,5 +1,4 @@
 export const Niche = `id,assignment,name,pronouns,hometown,year,majorMinor,info,email,focus
 jct324,"Niche","Joss Tripoli",they/them,"Perkasie, PA",2023,"Computer Science, Japanese","Hello! Some things I like to do are listen to music, juggle, and make art. Software and web development are passions of mine, so I am excited to be working on the TRAC website this semester. I hope to improve the website's branding & consistency, presence, information, and functionality. Previously I have TRAC'ed BUS 003 and BIOS 042.",jct324@lehigh.edu,"Website"
-missing,"Niche","Bisceglia",‎,"",‎,"","",missing@lehigh.edu,"Alumni"
-hideMe,"Niche","",‎,"",‎,"","",missing@lehigh.edu,"Speaking Fellows"`;
+missing,"Niche","Bisceglia",‎,"",‎,"","",missing@lehigh.edu,"Alumni"`;
 
--- fellowData/SOC197.js
+++ fellowData/SOC197.js
@@ -1,7 +1,6 @@
 export const SOC197 = `id,assignment,name,pronouns,hometown,year,majorMinor,info,email
 frh223,"SOC 197","Frannie Hottinger",she/her,"Inver Grove Heights",2023,"Management Consulting major, Business Information Systems minor","I am also on the women's basketball team at Lehigh and was able to spend this past summer interning Barcelona. One of my favorite parts of the TRAC program is that it has allowed me to create new connections and friends with people in the Lehigh community I would not have come across in my normal schedule.",frh223@lehigh.edu
-gek224,"SOC 197","Gabrielle Kesh",she/her,"North Caldwell, NJ",2024,"Major: Management Minor: Sociology","I am a junior at Lehigh who is studies business, loves writing, and enjoys having fun with friends! A fun fact about me is that I have a younger brother and that I am allergic to peanuts. A TV show I recommend is ""Glee"" because it is my favorite show and the music is incredible. I decided to join TRAC to immerse myself in a community of hard-working, talented, and passionate individuals! ",gek224@lehigh.edu
-hideMe,"HIST 001","",‎,"",‎,"","",mwd223@lehigh.edu`;
+gek224,"SOC 197","Gabrielle Kesh",she/her,"North Caldwell, NJ",2024,"Major: Management Minor: Sociology","I am a junior at Lehigh who is studies business, loves writing, and enjoys having fun with friends! A fun fact about me is that I have a younger brother and that I am allergic to peanuts. A TV show I recommend is ""Glee"" because it is my favorite show and the music is incredible. I decided to join TRAC to immerse myself in a community of hard-working, talented, and passionate individuals! ",gek224@lehigh.edu`;
 
 
 
