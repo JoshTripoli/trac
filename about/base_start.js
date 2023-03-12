b50247c1aadf62ff19fcd9628ec5a048d81d6671
Josh Tripoli <jct324@lehigh.edu>
2023-03-12 03:38:20 -0400

    Prepare for conference section added

--- about/base_start.js
+++ about/base_start.js
@@ -26,22 +26,18 @@ function clickGotIt() {
         if ($(".keyTermDefinition:hidden").length === 0) {
             // All labels are clicked so everything is good to proceed
             $('#clickLabelsWarning').hide();
-            // actionArray should be empty if enableDataCollection = false
-            Promise.all(actionArray).then(function() {
-                let pathArray = window.location.pathname.split('/');
-                if (pathArray[2] === "privacy") {
-                    // special case for the privacy module
-                    window.location.href = '/tut_guide/' + pathArray[2];
-                } else {
-                    window.location.href = '/tutorial/' + pathArray[2];
-                }
-            });
+            // window.location.href = 'index.html';  
         } else {
             // User has not clicked all the labels - show warning and animate unclicked
             $('#clickLabelsWarning').show();
             animateUnclickedLabels();
         }
     }
+    if ($('.prepareSegment').is(":hidden")) {
+        // User has not yet clicked next
+        $('#clickNextWarning').show();
+        $('.showPrepareButton').transition('bounce');
+    } 
 };
 
 $(window).on("load", function() {
@@ -63,13 +59,31 @@ $(window).on("load", function() {
         }
     });
 
+    $('.showPrepareButton').on('click', function() {
+        if ($(".keyTermDefinition:hidden").length === 0) {
+            $('#clickLabelsWarning').hide();
+            // $('.ui.labeled.icon.button').addClass('green');
+            $('#clickNextWarning').hide();
+            $('.showPrepareButton').css('display', 'none');
+            $('.prepareSegment').show();
+            $('.prepareSegment').transition('jiggle');
+            if ($(".keyTermDefinition:hidden").length === 0) {
+                $('.ui.labeled.icon.button').addClass('green');
+            }
+        } else { // user needs to finish going through key ideas before moving on to preparing for conference section
+            $('#clickLabelsWarning').show();
+            animateUnclickedLabels();
+        }
+
+    });
+
     $('.keyTerm').on('click', function(event) {
         $(event.target).closest('.keyTerm').children('.keyTermDefinition').show();
         $(event.target).closest('.keyTerm').transition('tada');
-        if ($(".keyTermDefinition:hidden").length === 0) {
-            $('#clickLabelsWarning').hide();
-            $('.ui.labeled.icon.button').addClass('green');
-        }
+        // if ($(".keyTermDefinition:hidden").length === 0) {
+        //     $('#clickLabelsWarning').hide();
+        //     $('.ui.labeled.icon.button').addClass('green');
+        // }
         const vocabTerm = $(event.target).closest('.keyTerm').children('.keyTermLabel').text();
     });
 
